4e24be414ca84bbb345d9f7118059f86c7ea24ba
DESKTOP-HMKBFVT\USER <radtani8810@gmail.com>
2024-06-14 22:55:59 +0900

    first commit

---
+++ assets/js/slide.js
@@ -0,0 +1,36 @@
+"use strict";
+
+
+{
+    const images = [
+        'images/syokanokaze.jpg',
+        'images/bikan.jpg',
+        'images/megaraba.jpg',
+        'images/sitsunen.jpg',
+        'images/akusyu.jpg',
+        'images/zyama.jpg'
+    ];
+    const slideImage = document.getElementById('mainImage');
+    const nextButton = document.getElementById('nextImage');
+    const prevButton = document.getElementById('prevImage');
+
+    let current = 0;
+
+    mainImage.textContent = `1/${images.length}`;
+
+    nextButton.addEventListener('click', function() {
+        if (current + 1 < images.length) {
+            current++;
+            slideImage.src = images[current];
+        }
+        mainImage.textContent = `${current + 1}/${images.length}`;
+    });
+
+    prevButton.addEventListener('click', function() {
+        if (current > 0) {
+            current--;
+            slideImage.src = images[current];
+        }
+        mainImage.textContent = `${current + 1}/${images.length}`;
+    });
+}
