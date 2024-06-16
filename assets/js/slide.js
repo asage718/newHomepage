d1f663124c8dbbe3c0634a3ab1568b3c8fce5161
DESKTOP-HMKBFVT\USER <radtani8810@gmail.com>
2024-06-17 03:39:59 +0900

    commit

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,44 +1,15 @@
-"use strict";
-
-
-{
-    const images = [
-        'images/syokanokaze.jpg',
-        'images/bikan.jpg',
-        'images/megaraba.jpg',
-        'images/sitsunen.jpg',
-        'images/akusyu.jpg',
-        'images/zyama.jpg'
-    ];
-    const slideImage = document.getElementById('mainImage');
-    const nextButton = document.getElementById('nextImage');
-    const prevButton = document.getElementById('prevImage');
-
-    let current = 0;
-
-    mainImage.textContent = `1/${images.length}`;
-
-    nextButton.addEventListener('click', function() {
-        if (current + 1 < images.length) {
-            current++;
-            slideImage.src = images[current];
-        }
-        else if (current + 1 === images.length) {
-            current = 0;
-            slideImage.src = images[current];
-        }
-        mainImage.textContent = `${current + 1}/${images.length}`;
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: true,//自動的に動き出すか。初期値はfalse。
+        autoplaySpeed: 6000,//スライドの表示時間を6秒に設定
+        infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+        speed: 1000,//スライドのスピード。初期値は300。
+        slidesToShow: 3,//スライドを画面に3枚見せる
+        slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+        prevArrow: '#prevImage',//矢印部分PreviewのHTMLを変更
+        nextArrow: '#nextImage',//矢印部分NextのHTMLを変更
+        centerMode: true,//要素を中央ぞろえにする
+        variableWidth: true,//幅の違う画像の高さを揃えて表示
+        dots: true,//下部ドットナビゲーションの表示
     });
-
-    prevButton.addEventListener('click', function() {
-        if (current > 0) {
-            current--;
-            slideImage.src = images[current];
-        }
-        else if (current === 0) {
-            current = images.length - 1;
-            slideImage.src = images[current];
-        }
-        mainImage.textContent = `${current + 1}/${images.length}`;
-    });
-}
+});
